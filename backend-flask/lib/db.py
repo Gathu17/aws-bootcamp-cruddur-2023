02711b9e350369d3acf4fd71b8960c390136c288
Jerry Gathu <92152669+Gathu17@users.noreply.github.com>
2023-04-08 10:16:06 +0000

    seeded data to ddb, modified message group page

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -50,7 +50,7 @@ class Db:
         return json[0]
    # when we want to commit data such as an insert
   def query_commit(self,sql,params={}):
-    self.print_sql('commit with returning',sql)
+    self.print_sql('commit with returning',sql,params)
 
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
